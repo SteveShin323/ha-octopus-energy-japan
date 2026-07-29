063db771db5a4410c5bc236ebc2a7a1902cfa530
SteveShin323 <114638143+SteveShin323@users.noreply.github.com>
2026-07-29 12:04:32 +0900

    Add integration constants

---
+++ custom_components/octopus_energy_japan/const.py
@@ -0,0 +1,5 @@
+"""Constants for the Octopus Energy Japan integration."""
+
+DOMAIN = "octopus_energy_japan"
+API_URL = "https://api.oejp-kraken.energy/v1/graphql/"
+DEFAULT_SCAN_INTERVAL_MINUTES = 30
